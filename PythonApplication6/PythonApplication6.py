e3cfdb2d886a7cbc29cdc3eb5470f363dc2772cb
Михаил Бурков <mikhail.burkov2003@gmail.com>
2020-10-28 09:12:40 +0200

    Добавьте файлы проекта.

---
+++ PythonApplication6/PythonApplication6.py
@@ -0,0 +1,15 @@
+x = float(input("Введіть значення агрументу - "))
+A = float(input("Введіть значення константи - "))
+if x<0:
+       y = x
+       print(y)
+elif A>0: 
+        y = A 
+        print(y)
+else: 
+    print("Немає розв'язків")
+    
+
+
+
+
